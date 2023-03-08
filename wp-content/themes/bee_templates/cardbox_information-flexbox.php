fc4acf6b94ced2e07472cc4f257672783d98daa3
ega91 <44427684+ega91@users.noreply.github.com>
2023-03-08 08:59:02 +0700

    Update cardbox_information-flexbox.php
    cardbox information redesigned

--- wp-content/themes/bee_templates/cardbox_information-flexbox.php
+++ wp-content/themes/bee_templates/cardbox_information-flexbox.php
@@ -41,13 +41,19 @@
             <img src="wp-content/themes/bee_templates/assets/icon/block-header-3d-student.svg" alt="Future Students" width="150px" height="auto">
             <h3>Future Students</h3>
             <p>Get more information regarding joining us below!</p>
-            <div class="horizontal-flex">
-            <img src="wp-content/themes/bee_templates/assets/icon/block-header-3d-person.svg" alt="Gabung Binus" width="150px" height="auto">
-            <img src="wp-content/themes/bee_templates/assets/icon/block-header-3d-person.svg" alt="Tuition Info" width="150px" height="auto">
-            <img src="wp-content/themes/bee_templates/assets/icon/block-header-3d-person.svg" alt="Scholarship" width="150px" height="auto">
-            <p>Gabung Binus</p>
-            <p>Tuition Info</p>
-            <p>Scholarship</p>
+            <div class="horizontal-flex" style="width:100%;">
+                <table style="border-collapse: collapse;">
+                    <tr>
+                        <td><img style="width:50%; height:auto" src="wp-content/themes/bee_templates/assets/icon/block-header-3d-gabung-binus.svg" alt="Gabung Binus"></td>
+                        <td><img style="width:50%; height:auto" src="wp-content/themes/bee_templates/assets/icon/recognition.svg" alt="Tuition Info" ></td>
+                        <td><img style="width:50%; height:auto" src="wp-content/themes/bee_templates/assets/icon/block-header-3d-scholarship.svg" alt="Scholarship"></td>
+                    </tr>
+                    <tr>
+                        <td>Gabung Binus</td>
+                        <td>Tuition Info</td>
+                        <td>Scholarship</td>
+                    </tr>
+                </table>
             </div>
         </div>
     </div>
@@ -63,15 +69,15 @@
                 <span class="svgcon-search svg " style="background-image: none;"></span></form>
                 <p>Or you can filter based on degree and location before finding</p>
             <div class="horizontal-flex">
-                <select name="" id="" class="degree-option" placeholder="All Degree" style="">
+                <select name="" id="" class="degree-option" placeholder="All Degree" style="width:30%">
                 <option value="">All Degree</option>
                 <option value="d3" class="d3">D3</option><option value="d3-extension" class="d3-extension">D3 Extension</option><option value="d3-extension-online" class="d3-extension-online">D3 Extension Online</option><option value="d4" class="d4">D4</option><option value="double-programs" class="double-programs">Double Programs</option><option value="master-track" class="master-track">Master Track</option><option value="online-program" class="online-program">Online Program</option><option value="s1" class="s1">S1</option><option value="s1-online" class="s1-online">S1 Online</option><option value="s2" class="s2">S2</option><option value="s2-online" class="s2-online">S2 Online</option><option value="s3" class="s3">S3</option><option value="undergraduate-international-program" class="undergraduate-international-program">Undergraduate (International)</option></select>
 
-                <select name="" id="" class="degree-option" placeholder="All Degree" style="">
+                <select name="" id="" class="degree-option" placeholder="All Degree" style="width:30%">
                 <option value="">All Location</option>
                 <option value="d3" class="d3">D3</option><option value="d3-extension" class="d3-extension">D3 Extension</option><option value="d3-extension-online" class="d3-extension-online">D3 Extension Online</option><option value="d4" class="d4">D4</option><option value="double-programs" class="double-programs">Double Programs</option><option value="master-track" class="master-track">Master Track</option><option value="online-program" class="online-program">Online Program</option><option value="s1" class="s1">S1</option><option value="s1-online" class="s1-online">S1 Online</option><option value="s2" class="s2">S2</option><option value="s2-online" class="s2-online">S2 Online</option><option value="s3" class="s3">S3</option><option value="undergraduate-international-program" class="undergraduate-international-program">Undergraduate (International)</option></select>
 
-                <select name="" id="" class="degree-option" placeholder="All Degree" style="">
+                <select name="" id="" class="degree-option" placeholder="All Degree" style="width:30%">
                 <option value="">Find</option>
                 <option value="d3" class="d3">D3</option><option value="d3-extension" class="d3-extension">D3 Extension</option><option value="d3-extension-online" class="d3-extension-online">D3 Extension Online</option><option value="d4" class="d4">D4</option><option value="double-programs" class="double-programs">Double Programs</option><option value="master-track" class="master-track">Master Track</option><option value="online-program" class="online-program">Online Program</option><option value="s1" class="s1">S1</option><option value="s1-online" class="s1-online">S1 Online</option><option value="s2" class="s2">S2</option><option value="s2-online" class="s2-online">S2 Online</option><option value="s3" class="s3">S3</option><option value="undergraduate-international-program" class="undergraduate-international-program">Undergraduate (International)</option></select>
             </div>
@@ -87,15 +93,16 @@
         <p>Our programs are designed to shape future leaders. Explore our programs below.</p>
         <div class="horizontal-flex">
             <article>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci rem eos alias deleniti nobis ad tempora. Cupiditate, repellat. Odio accusantium sapiente magni adipisci pariatur veritatis fugiat, incidunt eveniet. Adipisci, pariatur.
+                <h4 style="text-align:left; font-size:21px; margin-left:10px;">Tony Devisa, Petani Muda Berkontribusi dalam Membina dan Memberdayakan Masyarakat<h4>
             </article>
-<br/>
+            
+<hr/>
             <article>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci rem eos alias deleniti nobis ad tempora. Cupiditate, repellat. Odio accusantium sapiente magni adipisci pariatur veritatis fugiat, incidunt eveniet. Adipisci, pariatur.
+            <h4 style="text-align:left; font-size:21px; margin-left:10px;">BINUS UNIVERSITY Pertahankan Posisi 1 di ASEAN pada AppliedHE Private University Ranking: ASEAN+ 2023</h4>
             </article>
-<br/>
+<hr/>
             <article>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci rem eos alias deleniti nobis ad tempora. Cupiditate, repellat. Odio accusantium sapiente magni adipisci pariatur veritatis fugiat, incidunt eveniet. Adipisci, pariatur.
+            <h4 style="text-align:left; font-size:21px; margin-left:10px;">Penandatanganan MOU BINUS UNIVERSITY dengan USAID HEPI. Penguatan Kolaborasi Pendidikan Tinggi Indonesia dengan Amerika Serikat</h4>
             </article>
         </div>
         <a href="https://binus.ac.id/program" style="color:white;" >View All News&#62;	</a>
@@ -104,9 +111,30 @@
     <div class="regular-space">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</div>
 
     <div class="info-card">
-        <img src="wp-content/themes/bee_templates/assets/icon/block-header-3d-calendar.svg" alt="Choose Your Role" width="150px" height="auto">
+        <img src="wp-content/themes/bee_templates/assets/icon/block-header-3d-calendar.svg" alt="event" width="150px" height="auto">
         <h3>Event</h3>
-        <p>Let us know more about you, choose which role fits you.</p>
+        <p>Here are our updates of the events related to BINUS University</p>
+        <table cellpadding="0" cellspacing="0" style="margin-bottom: 0px;">
+            <tr>
+                <td><img style="width:50%; height:auto" src="wp-content/themes/bee_templates/assets/icon/block-header-3d-calendar.svg"></td>
+                <td><article>
+                <h4 style="text-align:left; font-size:21px; margin-left:10px;">Introduction to Model-Agnostic Meta-Learning<h4>
+            </article><hr/></td>
+            </tr>
+            <tr>
+                <td><img style="width:50%; height:auto" src="wp-content/themes/bee_templates/assets/icon/block-header-3d-calendar.svg"></td>
+                <td><article>
+                <h4 style="text-align:left; font-size:21px; margin-left:10px;">Cyber Security for Technopreneurship<h4>
+            </article><hr/></td>
+            </tr>
+            <tr>
+                <td><img style="width:50%; height:auto" src="wp-content/themes/bee_templates/assets/icon/block-header-3d-calendar.svg"></td>
+                <td><article>    
+                <h4 style="text-align:left; font-size:21px; margin-left:10px;">Industrial Sharing: Entrepreneur<h4>
+                </article></td>
+            </tr>
+        </table>
+
         <a href="https://binus.ac.id/program" style="color:white;" >View All Event&#62;	</a>
     </div>
 </div>
