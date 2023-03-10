264656b7333304d2d8e1b3255ec2abb581a184ef
ega91 <44427684+ega91@users.noreply.github.com>
2023-03-10 14:58:13 +0700

    footer redesign
    footer redesigned to be same as the original web

--- wp-content/themes/bee_templates/footer.php
+++ wp-content/themes/bee_templates/footer.php
@@ -1,10 +1,34 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+
 <?php 
 /**
  * Override parent header.php 
  */
-echo('<div id=footer>
-<h1>&copy 2023 - Bee Dumy </h1>
-</div>')
+// echo('<div id=footer>
+// <h1>&copy 2023 - Bee Dumy </h1>
+// </div>')
 
 ?>
 <!-- Your custom header content here -->
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+<div id="footer">
+    <div class="vertical-flex" style="text-align: center;">
+        <div class="horizontal-flex" style="width:100%; text-align: left;">
+            <i class="fas fa-map-marker-alt"></i>
+            <p style="padding-left: 20px">jl. K.H. Syahdan No. 9, Kemanggisan, Palmerah Jakarta 11480 Indonesia</p>
+        </div>
+        <div class="horizontal-flex" style="text-align:left; width:100%">
+            <i class="fas fa-phone"></i>
+            <p style="padding-left: 20px; text-align:left">Phone +62 21 534 5830, +62 21 535 0660 Fax +62 21 530 0244</p>
+        </div>
+        <div class="horizontal-flex" style="text-align:left; width:15%">
+            <img class="flex-space" src="wp-content/themes/bee_templates/assets/icon/icon-facebook.svg" alt="facebook" width="32px" height="auto"></img>
+            <img class="flex-space" src="wp-content/themes/bee_templates/assets/icon/icon-instagram.svg" alt="instagram" width="32px" height="auto"></img>
+            <img class="flex-space" src="wp-content/themes/bee_templates/assets/icon/icon-twitter.svg" alt="twitter" width="32px" height="auto"></img>
+        </div>
+        <hr/>
+        <p style="text-align: center; padding-bottom: 0px;">Copyright © BINUS Higher Education. All rights reserved</p>
+    </div>
+</div>
